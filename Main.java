2ece88468960f636549b4fa34c1ac4fad5668372
jjaekkaemi <35017054+jjaekkaemi@users.noreply.github.com>
2018-12-02 23:39:48 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	final static int N = 15 ;
+	public static void main(String[] args) {
+		for(int i = -N; i<=N; i++) {
+			for(int j = -N ; j<= N ; j++) {
+				if((i*i+j*j) <= N*N) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println(); 
+		}
+
+	}
+
+}
